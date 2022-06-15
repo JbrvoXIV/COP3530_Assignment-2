a74a2827acba85c8c2801039224012d99e365332
JbrvoXIV <jbravo0423@gmail.com>
2022-06-14 20:21:29 -0400

    renamed

---
+++ Balanced_Unbalanced-Part-II/OrderedKeyValue.java
@@ -0,0 +1,17 @@
+
+public class OrderedKeyValue implements Comparable{
+    String key;
+    int value;
+    @Override
+    public int compareTo(Object o) {
+	return key.compareTo(((OrderedKeyValue)o).key);
+    }
+    public OrderedKeyValue(String key, int value) {
+	this.key = key;
+	this.value = value;
+    }
+    @Override
+    public String toString() {
+	return "(" + key + ", " + value + ")";
+    }
+}
